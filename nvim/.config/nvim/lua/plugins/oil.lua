c33c8f879cb374c6c950a4851933f216589b7176
Kenny Mecham <polkm105@gmail.com>
2026-08-15 23:00:50 -0700

    Oil.nvim floating window changes

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -6,13 +6,26 @@ return {
     { 'echasnovski/mini.icons', opts = {} },
   },
   keys = {
-    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
+    { '-', '<CMD>Oil --float<CR>', desc = 'Open parent directory' },
   },
   opts = {
     -- Your custom oil configurations go here
     default_file_explorer = true,
     view_options = {
       show_hidden = true,
+    },
+    skip_confirm_for_simple_edits = true,
+    float = {
+      border = 'rounded',
+      padding = 2,
+      -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      max_width = 0.75,
+      max_height = 0.85,
+
+      override = function(conf)
+        conf.row = math.floor((vim.o.lines - conf.height) / 2) - 2
+        conf.col = math.floor((vim.o.columns - conf.width) / 2) - 1
+      end,
     },
   },
 }
