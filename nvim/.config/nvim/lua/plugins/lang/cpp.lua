a7361d6584c13d98bbae21ab4bff96ef59b3d2d9
Kenny Mecham <polkm105@gmail.com>
2026-08-17 00:15:03 -0700

    Formatting and treesitter fix

--- nvim/.config/nvim/lua/plugins/lang/cpp.lua
+++ nvim/.config/nvim/lua/plugins/lang/cpp.lua
@@ -1,14 +1,29 @@
-vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("CppBuildCommands", { clear = true }),
-	pattern = { "c", "cpp" },
-	callback = function()
-		vim.cmd("compiler gcc")
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('CppBuildCommands', { clear = true }),
+  pattern = { 'c', 'cpp' },
+  callback = function()
+    vim.cmd 'compiler gcc'
 
-		vim.bomakeprg = "make"
-		vim.keymap.set("n", "<leader>cb", "<cmd>make all<CR>", { buffer = true, desc = "[C]ompile and [B]uild" })
-		vim.keymap.set("n", "<leader>cp", "<cmd>make<CR>", { buffer = true, desc = "[C]ompile [P]roject" })
-		vim.keymap.set("n", "<leader>cf", "<cmd>make -c % -o %:r.o<CR>", { buffer = true, desc = "[C]ompile [F]ile" })
-	end,
+    vim.bomakeprg = 'make'
+    vim.keymap.set(
+      'n',
+      '<leader>cb',
+      '<cmd>make all<CR>',
+      { buffer = true, desc = '[C]ompile and [B]uild' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>cp',
+      '<cmd>make<CR>',
+      { buffer = true, desc = '[C]ompile [P]roject' }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>cf',
+      '<cmd>make -c % -o %:r.o<CR>',
+      { buffer = true, desc = '[C]ompile [F]ile' }
+    )
+  end,
 })
 
 return {}
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -491,8 +491,9 @@ return {
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
